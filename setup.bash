9b8f5b6b1541b6ccba0612c70b2d60cb46bed975
LittleNefertum <121692110+LittleNefertum@users.noreply.github.com>
2022-12-31 14:15:53 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "******************************"
+echo "*** Installing zip & unzip ***"
+echo "******************************"
 sudo apt install zip
 sudo apt install unzip
+echo "******************************"
+echo pwd
+echo "******************************"
 source ./awscliv2/setup.bash
