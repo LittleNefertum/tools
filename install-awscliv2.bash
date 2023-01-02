0a3934686534ff19a2e5b37269e4d20af63744d5
LittleNefertum <121692110+LittleNefertum@users.noreply.github.com>
2023-01-02 18:41:25 +0100

    Update install-awscliv2.bash

--- install-awscliv2.bash
+++ install-awscliv2.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p "$HOME/Downloads"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/Downloads/awscliv2.zip"
-unzip "$HOME/Downloads/awscliv2.zip" -o -d "$HOME/Downloads"
+unzip "$HOME/Downloads/awscliv2.zip" -d "$HOME/Downloads"
 rm "$HOME/Downloads/awscliv2.zip"
 sudo "$HOME/Downloads/aws/install" --update
 # rm -rf "$HOME/Downloads/aws"
