70c8938a5432f1d0179fd139beedb2a085163ccb
LittleNefertum <121692110+LittleNefertum@users.noreply.github.com>
2023-01-02 16:05:02 +0100

    Create test.bash

---
+++ test.bash
@@ -0,0 +1,1 @@
+for i in 1.25.{0..1} ;do echo downloading kubectl $i;mkdir -p $HOME/kubectl/v$i;curl -o $HOME/kubectl/v$i/kubectl -sLOC - https://dl.k8s.io/release/v$i/bin/linux/amd64/kubectl; echo downloading kubectl.sha256 for kubectl $i;curl -o $HOME/kubectl/v$i/kubectl.sha256 -sLO https://dl.k8s.io/release/v$i/bin/linux/amd64/kubectl.sha256;done;
